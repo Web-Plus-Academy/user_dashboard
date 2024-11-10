2ca2389de7afdcdec024d6367f155d3e4fc5c640
saranthecoder <115061220+saranthecoder@users.noreply.github.com>
2024-11-11 01:12:51 +0530

    task submission

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -5,7 +5,7 @@ import './Dashboard.css';
 const Dashboard = ({ userDetails }) => {
   return (
     <>
-    <div className="dashboard-container">
+    <div className="dashboard-container ">
       <h4 className="dashboard-welcome">Welcome,</h4>
       <h1 className="dashboard-name">{userDetails.name}</h1>
       <p className="dashboard-id">ID: {userDetails.ID}</p>
